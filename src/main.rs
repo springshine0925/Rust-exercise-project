3102f100fe444c17150b209bfc80d4295aeee047
springshine0925 <wilsonchan@gmail.com>
2024-05-02 18:15:30 +0800

    main func again

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-// fn main() {
-//     let action = std::env::args().nth(1).expect("Please secify an action");
-//     let item=std::env::args().nth(2).expect("Please specify an item");
-//     println!("{:?}and{:?}", action, item);
-// }
-use std::collections::HashMap;
-
-struct Todo{
-    map: HashMap<String, bool>;
-    fn insert(&mut self, key:String){
-         self.map.insert(key,true);
-    }
+fn main() {
+    let action = std::env::args().nth(1).expect("Please secify an action");
+    let item=std::env::args().nth(2).expect("Please specify an item");
+    println!("{:?}and{:?}", action, item);
 }
+// use std::collections::HashMap;
+
+// struct Todo{
+//     map: HashMap<String, bool>;
+//     fn insert(&mut self, key:String){
+//          self.map.insert(key,true);
+//     }
+// }
