6fac8d99c6c6638c4985758d8a432b824d0dbbdb
springshine0925 <wilsonchan@gmail.com>
2024-05-06 14:54:23 +0800

    invalid Array Element Access

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::io;
 fn main() {
-    let action = std::env::args().nth(1).expect("Please secify an action");
-    let item=std::env::args().nth(2).expect("Please specify an item");
-    println!("{:?}and{:?}", action, item);
+   let a=[1,2,3,4,5];
+   println!("please enter an array index.");
+   let mut index =String::new();
+   io::stdin().read_line(&mut index).expect("Failed to read line");
+   let index:usize=index.trim().parse().expect("index entered was not a number");
+   let element=a[index];
+   println!("The value of the element at index {index} is : {element}");
+
 }
 // use std::collections::HashMap;
 
